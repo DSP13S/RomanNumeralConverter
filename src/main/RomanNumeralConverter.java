84e6b9a07bd4393ee883a2b1a3d48af00719b532
Tommy <Tnnguyen2@bsu.edu>
2022-01-24 02:41:02 -0500

    Tree Map Numbers 1 to 10

--- src/main/RomanNumeralConverter.java
+++ src/main/RomanNumeralConverter.java
@@ -1,5 +1,17 @@
 public class RomanNumeralConverter {
 
-    Private static TreeMap <Integer, String>  = new TreeMap<Integer, String>();
+    Private static TreeMap <Integer, String> romanNumeral = new TreeMap<Integer, String>();
+
+    romanNumeral.put(10, "X");
+    romanNumeral.put(9, "IX");
+    romanNumeral.put(8, "VIII");
+    romanNumeral.put(7, "VII");
+    romanNumeral.put(6, "VI");
+    romanNumeral.put(5, "V");
+    romanNumeral.put(4, "IV");
+    romanNumeral.put(3, "III");
+    romanNumeral.put(2, "II");
+    romanNumeral.put(1, "I");
+
 
 }
