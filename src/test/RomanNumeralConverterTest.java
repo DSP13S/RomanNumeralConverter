0ffafe96b175e8d59955759dff6a4d072e3d6fe8
Tommy <Tnnguyen2@bsu.edu>
2022-01-24 02:57:21 -0500

    Convert Roman Numeral

--- src/test/RomanNumeralConverterTest.java
+++ src/test/RomanNumeralConverterTest.java
@@ -1,5 +1,5 @@
 public void RomanNumeralConverterTest() {
     for (int i = 1; i <= 11; i++)
-        System.out.printlnm(i+"="+ RomanNumber.toRoman(i))
+        System.out.printlnm(i+"="+ RomanNumber.toRomanNumeral(i))
 
 }
